9fe46f7693c02b75ba9d522c87f57bbe562e2896
emperor006 <mauricecharles1000@gmail.com>
2023-09-28 12:23:37 +0100

    completed created home screen

--- app/src/main/java/com/app/weekls/Home.kt
+++ app/src/main/java/com/app/weekls/Home.kt
@@ -1,23 +1,364 @@
 package com.app.weekls
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.ListItem
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun Home() {
-    return Column(     modifier = Modifier
-        .fillMaxWidth()
-        .fillMaxHeight()
-        .background(color = Color(0xFF1A1824))) {
-        scoreboard()
+
+    return Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+            .background(color = Color(0xFF1A1824))
+            .padding(20.dp),
+    ) {
+        Scoreboard()
+        Spacer(modifier = Modifier.height(20.dp))
+        Text(
+            modifier = Modifier.fillMaxWidth(),
+            textAlign = TextAlign.Center,
+            text = "Task",
+            style = TextStyle(
+                color = Color.White, fontWeight = FontWeight.Bold, fontSize = 20.sp
+            )
+        )
+        Spacer(modifier = Modifier.height(20.dp))
 
 
+        Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+
+            ListItem(
+                text = {
+                    Text(
+                        text = "It has to be my way or no other way",
+                        style = TextStyle(color = Color.White)
+                    )
+                },
+                trailing = {
+                    Row() {
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(
+                                text = "+",
+                                style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.width(20.dp))
+
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(
+                                text = "-",
+                                style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)
+                            )
+
+                        }
+
+                    }
+
+                }
+            )
+            Spacer(modifier = Modifier.height(14.dp))
+            ListItem(
+                text = {
+                    Text(
+                        text = "Completing calendar work today",
+                        style = TextStyle(color = Color.White)
+                    )
+                },
+                trailing = {
+                    Row() {
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(
+                                text = "+",
+                                style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.width(20.dp))
+
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(
+                                text = "-",
+                                style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)
+                            )
+
+                        }
+
+                    }
+
+                }
+            )
+            Spacer(modifier = Modifier.height(14.dp))
+
+            ListItem(
+                text = {
+                    Text(
+                        text = "Goofy laughing or Joke",
+                        style = TextStyle(color = Color.White)
+                    )
+                },
+                trailing = {
+                    Row() {
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(
+                                text = "+",
+                                style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.width(20.dp))
+
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(
+                                text = "-",
+                                style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)
+                            )
+
+                        }
+
+                    }
+
+                }
+            )
+            Spacer(modifier = Modifier.height(14.dp))
+
+            ListItem(
+                text = {
+                    Text(
+                        text = "Putting someone in their place",
+                        style = TextStyle(color = Color.White)
+                    )
+                },
+                trailing = {
+                    Row() {
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(
+                                text = "+",
+                                style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.width(20.dp))
+
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(
+                                text = "-",
+                                style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)
+                            )
+
+                        }
+
+                    }
+
+                }
+            )
+
+            Spacer(modifier = Modifier.height(14.dp))
+
+            ListItem(
+                text = {
+                    Text(
+                        text = "Masculine Frame and Voice",
+                        style = TextStyle(color = Color.White)
+                    )
+                },
+                trailing = {
+                    Row() {
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(
+                                text = "+",
+                                style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.width(20.dp))
+
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(
+                                text = "-",
+                                style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)
+                            )
+
+                        }
+
+                    }
+
+                }
+            )
+
+            Spacer(modifier = Modifier.height(14.dp))
+
+            ListItem(
+                text = {
+                    Text(
+                        text = "Divide and assign task for others to do instead of doing it alone",
+                        style = TextStyle(color = Color.White)
+                    )
+                },
+                trailing = {
+                    Row() {
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(
+                                text = "+",
+                                style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.width(20.dp))
+
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(
+                                text = "-",
+                                style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)
+                            )
+
+                        }
+
+                    }
+
+                }
+            )
+
+            Spacer(modifier = Modifier.height(14.dp))
+
+
+            ListItem(
+                text = {
+                    Text(
+                        text = "No Youtube or social media",
+                        style = TextStyle(color = Color.White)
+                    )
+                },
+                trailing = {
+                    Row() {
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(
+                                text = "+",
+                                style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.width(20.dp))
+
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(
+                                text = "-",
+                                style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)
+                            )
+
+                        }
+
+                    }
+
+                }
+            )
+
+            Spacer(modifier = Modifier.height(14.dp))
+
+            ListItem(
+                text = {
+                    Text(
+                        text = "Did something scary",
+                        style = TextStyle(color = Color.White)
+                    )
+                },
+                trailing = {
+                    Row() {
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(
+                                text = "+",
+                                style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.width(20.dp))
+
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(
+                                text = "-",
+                                style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)
+                            )
+
+                        }
+
+                    }
+
+                }
+            )
+            Spacer(modifier = Modifier.height(14.dp))
+            ListItem(
+                text = {
+                    Text(
+                        text = "Hurt or offend others to satisfy any desire I have",
+                        style = TextStyle(color = Color.White)
+                    )
+                },
+                trailing = {
+                    Row() {
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(
+                                text = "+",
+                                style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.width(20.dp))
+
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(
+                                text = "-",
+                                style = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp)
+                            )
+
+                        }
+
+                    }
+
+                }
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { /*TODO remove everything he has ever scored
+    */
+
+                }) {
+                Text(text = "Missed a day")
+            }
+
+
+        }
 
 
     }
+
+}
+
+fun subtractScore(score: Any): Unit {
+
+
+}
+
+fun missedAday():Unit{
+
 }
--- app/src/main/java/com/app/weekls/scorecards.kt
+++ app/src/main/java/com/app/weekls/scorecards.kt
@@ -1,9 +1,9 @@
 package com.app.weekls
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.Text
@@ -11,20 +11,55 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
-fun scorecard() {
+fun Scoreboard() {
     return Card(
         modifier = Modifier
             .fillMaxWidth()
-            .height(300.dp),
-        shape = RoundedCornerShape(10.dp),
-        border = BorderStroke(1.dp, Color.Yellow),
+            .height(130.dp)
+            .background(color = Color(0xFF1A1824)),
+        shape = RoundedCornerShape(8.dp),
+        border = BorderStroke(1.dp, Color.Green),
+        backgroundColor = Color(0xFF1A1824),
         elevation = 6.dp
     ) {
         Column() {
-            Text(text = "3.0", style = TextStyle(color = Color.White))
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                text = "Sneakers and Clothes, Gym membership worth £1,000",
+                style = TextStyle(color = Color.White),
+                textAlign = TextAlign.Center
+            )
+
+//on days you are going to an activity or event that you might fall back to
+// old behavious, a functionality to increase the reward for those special days.
+// a guy that is looking to quit alcohol might increase the reward if he is stepping out for a friends birthday party
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(10.dp),
+                horizontalArrangement = Arrangement.Center
+
+            ) {
+                Text(text = "Score: ", style = TextStyle(color = Color.White, fontSize = 20.sp))
+                Text(
+                    text = "£0",
+                    style = TextStyle(
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White,
+                        fontSize = 20.sp
+                    )
+                )
+            }
+
 
         }
     }
